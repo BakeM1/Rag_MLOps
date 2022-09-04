89656e76b65ead1cac5af2a5b39095b071bb3c3e
BakeM1 <bakejobs4u@gmail.com>
2022-09-05 00:42:44 +0530

    Rag LR

--- train_data.py
+++ train_data.py
@@ -24,8 +24,8 @@ X = Normalizer().fit_transform(X)
 
 #Using a Logistic Regression in production
 #clf = LogisticRegression(solver=yaml.safe_load(open('params.yaml'))['solver'])
-clf = MultinomialNB()
-#clf = LinearDiscriminantAnalysis()
+#clf = MultinomialNB()
+clf = LinearDiscriminantAnalysis()
 y_pred = cross_val_predict(clf, X, y, cv = yaml.safe_load(open('params.yaml'))['cv'])
 
 # Metrics for comparing performance bw models
