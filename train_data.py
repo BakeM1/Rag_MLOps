d04286b02e4f61e92735de99164112efb051e103
BakeM1 <bakejobs4u@gmail.com>
2022-09-05 15:00:06 +0530

    changes with lrg to logistic

--- train_data.py
+++ train_data.py
@@ -67,9 +67,9 @@ X = df.to_numpy()
 X = Normalizer().fit_transform(X)
 
 #Using a Logistic Regression in production
-#clf = LogisticRegression(solver=yaml.safe_load(open('params.yaml'))['solver'])
+clf = LogisticRegression(solver=yaml.safe_load(open('params.yaml'))['solver'])
 #clf = MultinomialNB()
-clf = LinearDiscriminantAnalysis()
+#clf = LinearDiscriminantAnalysis()
 y_pred = cross_val_predict(clf, X, y, cv = yaml.safe_load(open('params.yaml'))['cv'])
 
 # Metrics for comparing performance bw models
